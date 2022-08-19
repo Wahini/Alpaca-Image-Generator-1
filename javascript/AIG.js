b159484e3599f03a6c3a73dbfa40b4516cc1ed43
Bhadrika Evandito Atmomintarso <42692320+Wahini@users.noreply.github.com>
2022-08-19 22:26:46 +0700

    update

--- javascript/AIG.js
+++ javascript/AIG.js
@@ -1,5 +1,5 @@
 // Primary Buttons select
-const background_btn = document.getElementById('background');
+const bg_btn = document.getElementById('background');
 const mayo_btn = document.getElementById('mayo');
 const nose_btn = document.getElementById('nose');
 const clot_btn = document.getElementById('cloth');
@@ -13,6 +13,7 @@ const mayo_section = document.getElementById('section_mayo');
 const nose_section = document.getElementById('section_nose');
 const cloth_section = document.getElementById('section_cloth');
 
+
 //All secondary buttons select acc to section
 const bgBtnAll = document.querySelectorAll('.btn-bg');
 const mayoBtnAll = document.querySelectorAll('.btn-mayo');
@@ -20,37 +21,20 @@ const noseBtnAll = document.querySelectorAll('.btn-nose');
 const clothBtnAll = document.querySelectorAll('.btn-cloth');
 
 //display & hide sections on primary button click
+
 function hide(ids){
     ids.forEach(id => {
       id.style.display ='none';
     });
   }
+
   function show(ids){
     ids.forEach(id => {
       id.style.display ='block';
     });
   }
 
-//   neck_btn.onclick = function() {
-
-//     show([style_section,
-//         seconday_heading,
-//         neck_section,
-//     ]);
-
-//     hide([
-//         background_section,
-//         ears_section,
-//         leg_section,
-//         hair_section,
-//         mouth_section,
-//         eyes_section,
-//         accessories_section
-//       ]);
-
-// }
-
-  background_btn.onclick = function() {
+bg_btn.onclick = function() {
     show([
         style_section, 
         seconday_heading, 
@@ -67,43 +51,45 @@ function hide(ids){
 mayo_btn.onclick = function() {
   show([style_section,
       seconday_heading,
-      mayo_section,
+      mayo_section
   ]);
 
   hide([
     background_section,
     nose_section,
-    cloth_section,
+    cloth_section
     ]);
 }
 
 nose_btn.onclick = function() {
   show([style_section,
       seconday_heading,
-      nose_section,
+      nose_section
   ]);
 
   hide([
     background_section,
     mayo_section,
-    cloth_section,
+    cloth_section
     ]);
 }
 
 cloth_btn.onclick = function() {
   show([style_section,
       seconday_heading,
-      cloth_section,
+      cloth_section
   ]);
 
   hide([
     background_section,
     mayo_section,
-    nose_section,
+    nose_section
     ]);
 }
 
 // Button state Active 
+
+
 //Image Change Js
 
 bgBtnAll.forEach(item => {
@@ -115,7 +101,7 @@ bgBtnAll.forEach(item => {
   })
   })
 
-mayoBtnAll.forEach(item => {
+  mayoBtnAll.forEach(item => {
     item.addEventListener('click', (event) => {
       document.getElementById('mayo_img').src = "/assets/mayo/mayo/" + event.target.value + ".png";
       
@@ -137,16 +123,16 @@ mayoBtnAll.forEach(item => {
   })
   })
 
-  // neckBtnAll.forEach(item => {
-  //   item.addEventListener('click', (event) => {
-  //     document.getElementById('neck_img').src = "/assets/alpaca/neck/" + event.target.value + ".png";
+  clothBtnAll.forEach(item => {
+    item.addEventListener('click', (event) => {
+      document.getElementById('cloth_img').src = "/assets/mayo/clothings/" + event.target.value + ".png";
       
-  //     neckBtnAll.forEach(e => {
-  //       e.classList.remove('active-bg');});
+      mayoBtnAll.forEach(e => {
+        e.classList.remove('active-bg');});
         
-  //       event.target.classList.add('active-bg');
-  // })
-  // })
+        event.target.classList.add('active-bg');
+  })
+  })
 
   document.querySelectorAll('.btn-acc').forEach(item => {
     item.addEventListener('click', (event) => {
